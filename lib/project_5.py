84ca739fcab423d2fa3ca14eb62d327978e5110f
David Sanford <drdavid.sanford@gmail.com>
2017-01-17 15:21:07 -0800

    Updates

--- lib/project_5.py
+++ lib/project_5.py
@@ -1,5 +1,9 @@
 def load_data_from_database():
-    pass
+    from sqlalchemy import create_engine
+    import pandas as pd
+    connect_param = 'postgresql://dsi_student:correct horse battery staple@joshuacook.me:5432/dsi'
+    engine = create_engine(connect_param)
+    return pd.read_sql("SELECT * FROM madelon", con=engine)
 
 def make_data_dict():
     pass
